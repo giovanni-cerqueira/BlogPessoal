129ba5fc9b4d18738e5e3b437390663c2d213d5a
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-08-11 15:09:12 -0300

    Order by funcionando

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,13 @@
+import { AlertasService } from './../service/alertas.service';
 import { AuthService } from './../service/auth.service';
-import { TemaService } from './../service/tema.service';
-import { Router } from '@angular/router';
-import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
-import { PostagemService } from '../service/postagem.service';
 import { Tema } from '../model/Tema';
 import { User } from '../model/User';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -25,33 +26,31 @@ export class InicioComponent implements OnInit {
   user: User = new User()
   idUser = environment.id
 
+  key = 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
     private temaService: TemaService,
-    private authService: AuthService
-
-
+    private authService: AuthService,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
 
-    window.scroll(0,0)
-
     if(environment.token == ''){
-     /* alert('Sua seção expirou, faça login novamente!')*/
+      alert('Faça o login')
       this.router.navigate(['/entrar'])
     }
 
-    this.getAllTema()
+    this.getAllTemas()
     this.getAllPostagens()
-
   }
 
-  getAllTema(){
+  getAllTemas(){
     this.temaService.getAllTema().subscribe((resp: Tema[]) => {
       this.listaTemas = resp
-
     })
   }
 
@@ -61,31 +60,32 @@ export class InicioComponent implements OnInit {
     })
   }
 
-getAllPostagens(){
-  this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
-    this.listaPostagens = resp
-  })
-}
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
+  }
 
-findByIdUser(){
-  this.authService.getByIdUser(this.idUser).subscribe((resp: User) => {
-    this.user = resp
-  })
-}
+  findByIdUser(){
+    this.authService.getByIdUser(this.idUser).subscribe((resp: User) => {
+      this.user = resp
+    })
+  }
 
-
-publicar(){
+  publicar(){
     this.tema.id = this.idTema
     this.postagem.tema = this.tema
 
     this.user.id = this.idUser
     this.postagem.usuario = this.user
 
+
     this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
-    this.postagem = resp
-    alert('Postagem realizada com sucesso!')
-    this.postagem = new Postagem()
-    this.getAllPostagens()
+      this.postagem = resp
+      console.log(this.postagem)
+      this.alertas.showAlertSuccess('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
     })
 
   }
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -18,22 +18,22 @@ export class TemaService {
   }
 
   getAllTema(): Observable<Tema[]>{
-    return this.http.get<Tema[]>('https://blogdogio.herokuapp.com/temas', this.token)
+    return this.http.get<Tema[]>('https://blogdogio.herokuapp.com/temas')
   }
 
   getByIdTema(id: number): Observable<Tema>{
-    return this.http.get<Tema>(`https://blogdogio.herokuapp.com/temas/${id}`, this.token)
+    return this.http.get<Tema>(`https://blogdogio.herokuapp.com/temas/${id}`)
   }
 
   postTema(tema: Tema): Observable<Tema>{
-    return this.http.post<Tema>('https://blogdogio.herokuapp.com/temas', tema, this.token)
+    return this.http.post<Tema>('https://blogdogio.herokuapp.com/temas', tema)
   }
 
   putTema(tema: Tema): Observable<Tema>{
-    return this.http.put<Tema>('https://blogdogio.herokuapp.com/temas', tema, this.token)
+    return this.http.put<Tema>('https://blogdogio.herokuapp.com/temas', tema)
   }
 
   deleteTema(id: number) {
-    return this.http.delete(`https://blogdogio.herokuapp.com/temas/${id}`, this.token)
+    return this.http.delete(`https://blogdogio.herokuapp.com/temas/${id}`)
   }
 }
